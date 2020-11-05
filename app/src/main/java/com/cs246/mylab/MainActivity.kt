35b459fefb09f7a09c53a6ad5e126181ce45b4f9
LanOr <alano@ezsynergy.com>
2020-11-05 18:04:15 -0400

    initial android studio project

---
+++ app/src/main/java/com/cs246/mylab/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cs246.mylab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
